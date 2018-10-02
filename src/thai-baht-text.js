93c57c3fcda663a99cec053e65695341eab1f602
nathawat <n.thumthiwong@gmail.com>
2018-10-02 20:06:41 +0700

    refactory code for function get text unit

--- src/thai-baht-text.js
+++ src/thai-baht-text.js
@@ -24,28 +24,53 @@ const getIntegerDigits = (numberInput) => {
 	return numberInput.split('.')[0]
 }
 
+const reverseNumber = (number) => {
+	const numberStr = number.toString()
+	return numberStr.split('').reverse().join('')
+}
+
+const isZeroValue = (number) => {
+	return number == 0
+}
+
+const isUnitPostition = (position) => {
+	return position == 0
+}
+
+const isMillionsPosition = (position) => {
+	return (position >= 6 && position % 6 === 0)
+}
+
+const getBathUnit = (position, number) => {
+
+	let unitText = ""
+
+	if (!isUnitPostition(position)) {
+		unitText = unitsText[Math.abs(position - 1) % 6]
+	}
+
+	if( isZeroValue(number) && !isMillionsPosition(position)){
+		unitText = ""
+	}
+
+	return unitText
+}
+
 // convert function without async
 const convert = (numberInput) => {
-	let numberStr = numberInput.toString()
-	numberStr = numberStr.split('').reverse().join('')
 
+	const numberReverse = reverseNumber(numberInput)
 	let textOutput = ''
 
-	numberStr.split('').map((number, i) => {
+	numberReverse.split('').map((number, i) => {
 		const currentNumber = Number(number)
 		let numberText = numbersText[currentNumber]
-		let unitText = ''
 
-		if (i !== 0) {
-			unitText = unitsText[Math.abs(i - 1) % 6]
-		}
-
+		let unitText
 		if (i % 6 === 1 && currentNumber <= 2) {
 			if (currentNumber === 2) {
-				unitText = 'สิบ'
 				numberText = 'ยี่'
 			} else if (i > 6 && currentNumber === 1) {
-				unitText = 'สิบ'
 				numberText = ''
 			} else {
 				numberText = ''
@@ -54,13 +79,13 @@ const convert = (numberInput) => {
 
 		if (i >= 6 && i % 6 === 0) {
 			if (currentNumber === 1) {
-				if (i + 1 < numberStr.length) {
+				if (i + 1 < numberReverse.length) {
 					numberText = 'เอ็ด'
 				}
 			}
 		}
 
-		if (numberStr.length > 1 && i === 0 && currentNumber === 1) {
+		if (numberReverse.length > 1 && i === 0 && currentNumber === 1) {
 			numberText = 'เอ็ด'
 		}
 
@@ -69,6 +94,7 @@ const convert = (numberInput) => {
 			numberText = ''
 		}
 
+		unitText = getBathUnit(i, number)
 		textOutput = numberText + unitText + textOutput
 		return number
 	})
@@ -88,7 +114,6 @@ const convertFullMoney = (numberInput) => {
 	} else {
 		textOutput = `${textOutput}${primaryCurrency}${fullMoney}`
 	}
-
 	return textOutput
 }
 
